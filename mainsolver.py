8fb324e9963e42d1312f20b822e03f5ca58232fe
Alexsorokkin <Alex18041991SOS>
2018-12-11 20:15:05 +0300

    Добавлена история решённых уравнений

--- mainsolver.py
+++ mainsolver.py
@@ -32,7 +32,7 @@ class Ui_MainWindow(object):
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.tab)
-        self.label_3.setGeometry(QtCore.QRect(160, 60, 121, 21))
+        self.label_3.setGeometry(QtCore.QRect(160, 60, 150, 21))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label_3.setFont(font)
@@ -43,7 +43,7 @@ class Ui_MainWindow(object):
         self.tab_2.setObjectName("tab_2")
         self.label_4 = QtWidgets.QLabel(self.tab_2)
         self.label_4.setGeometry(QtCore.QRect(10, 0, 331, 291))
-        self.label_4.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_4.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         self.label_4.setObjectName("label_4")
         self.tabWidget.addTab(self.tab_2, "")
         self.tab_4 = QtWidgets.QWidget()
@@ -56,7 +56,7 @@ class Ui_MainWindow(object):
         self.tab_3.setObjectName("tab_3")
         self.label_5 = QtWidgets.QLabel(self.tab_3)
         self.label_5.setGeometry(QtCore.QRect(0, 0, 351, 281))
-        self.label_5.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_5.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         self.label_5.setObjectName("label_5")
         self.pushButton_2 = QtWidgets.QPushButton(self.tab_3)
         self.pushButton_2.setGeometry(QtCore.QRect(234, 0, 111, 23))
@@ -79,10 +79,10 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         self.pushButton.setText(_translate("MainWindow", "Решить"))
         self.label.setText(_translate("MainWindow", "Инструкция к вводу. \n"
-" 1) Ввод цифр и знаков производить через пробел. \n"
-" 2) Неизвестную принять за x (англ). \n"
-" 3) Квадрат неизвестной: x^(2) \n"
-" Пример ввода: 4x^(2) + 4x - 3 = 1"))
+                                                    " 1) Ввод цифр и знаков производить через пробел. \n"
+                                                    " 2) Неизвестную принять за x (англ). \n"
+                                                    " 3) Квадрат неизвестной: x^(2) \n"
+                                                    " Пример ввода: 4x^(2) + 4x - 3 = 1"))
         self.label_2.setText(_translate("MainWindow", "Ответ:"))
         self.label_3.setText(_translate("MainWindow", "..."))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Калькулятор"))
@@ -94,12 +94,19 @@ class Ui_MainWindow(object):
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "История"))
 
 
-
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
+        self.pushButton_2.clicked.connect(self.delhistory)
+
+    def delhistory(self):
+        f = open('History.txt', mode='w')
+        self.label_5.setText('История пуста')
+        f.write('')
+        f.close()
+        return
 
     def run(self):
         equation = self.lineEdit.text()
@@ -131,6 +138,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                 polozhit = False
             elif 'x^(2)' in i:
                 chislo = i[:-5]
+                if chislo == '':
+                    chislo = '1'
                 try:
                     chislo = int(chislo)
                 except ValueError:
@@ -143,6 +152,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                     kvadrx.append(chislo)
             elif 'x' in i:
                 chislo = i[:-1]
+                if chislo == '':
+                    chislo = '1'
                 try:
                     chislo = int(chislo)
                 except ValueError:
@@ -168,7 +179,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             b += i
         for i in svob_chl:
             c += i
-        discr = b*b - 4*a*c
+        discr = b * b - 4 * a * c
         oneotvet = True
         x1 = 0
         x2 = 0
@@ -180,6 +191,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                 self.label_3.setText("Любой X.")
                 solve = '\n'.join(solve)
                 self.label_4.setText(solve)
+                self.history(equation, "Любой X.")
                 return
             else:
                 solve.append(''.join([str(solvenum), ') ', 'Переносим всё в левую часть и сокращаем подобное.']))
@@ -190,14 +202,16 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                 solve.append(''.join([str(solvenum), ') ', 'Вычисляем x.']))
                 solve.append(''.join(['x = ', str(-c), '/', str(b)]))
                 x1 = str(x1)
+                text = " ".join(['x =', x1])
                 self.label_3.setText(" ".join(['x =', x1]))
                 solve = '\n'.join(solve)
                 self.label_4.setText(solve)
+                self.history(equation, text)
                 return
         solve.append(''.join([str(solvenum), ') ', 'Переносим всё в левую часть и сокращаем подобное.']))
         solvenum += 1
         solve.append('...')
-        solve.append(''.join([str(solvenum),') ', 'Вычисляем дискриминант.']))
+        solve.append(''.join([str(solvenum), ') ', 'Вычисляем дискриминант.']))
         solve.append('Формула дискриминанта: D = b*b - 4*a*c')
         solve.append(''.join(['D = ', str(discr)]))
         solvenum += 1
@@ -207,11 +221,12 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             solve.append(''.join([str(solvenum), ') ', 'Для такого D нет решения.']))
             solve = '\n'.join(solve)
             self.label_4.setText(solve)
+            self.history(equation, "Нет ответа.")
             return
         if discr == 0:
             solve.append(''.join([str(solvenum), ') ', 'Вычисляем единственный х.']))
             solve.append(''.join(['x = ', str(-b), '/(2*', str(a), ')']))
-            x1 = -b / 2*a
+            x1 = -b / 2 * a
             x1 = round(x1, 3)
             x1 = str(x1)
             solve.append(''.join(['x = ', x1]))
@@ -220,20 +235,55 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         elif discr > 0:
             oneotvet = False
             solve.append(''.join([str(solvenum), ') ', 'Вычисляем х1 и x2.']))
-            x1 = (-b + math.sqrt(discr)) / (2*a)
+            x1 = (-b + math.sqrt(discr)) / (2 * a)
             x1 = round(x1, 3)
             x1 = str(x1)
             solve.append(''.join(['x1 = (', str(-b), '+ sqrt(', str(discr), '))/(2*', str(a), ') = ', x1]))
-            x2 = (-b - math.sqrt(discr)) / (2*a)
+            x2 = (-b - math.sqrt(discr)) / (2 * a)
             x2 = round(x2, 3)
             x2 = str(x2)
             solve.append(''.join(['x2 = (', str(-b), '- sqrt(', str(discr), '))/(2*', str(a), ') = ', x2]))
             solve = '\n'.join(solve)
             self.label_4.setText(solve)
         if oneotvet:
+            text = ' '.join(['x =', x1])
             self.label_3.setText(' '.join(['x =', x1]))
+            self.history(equation, text)
         else:
+            text = ' '.join(['x1 =', x1, 'x2 =', x2])
             self.label_3.setText(' '.join(['x1 =', x1, 'x2 =', x2]))
+            self.history(equation, text)
+
+    def history(self, eq='', ans=''):
+        f = open('History.txt', mode='r')
+        h_data = f.read()
+        f.close()
+        f = open('History.txt', mode='w')
+        text = []
+        if eq == '' and ans == '':
+            self.label_5.setText('История пуста')
+            f.write('')
+            return
+        h_data = h_data.split('\n')
+        h_data.append('@#$'.join([eq, ans]))
+        schet = len(h_data)
+        if schet >= 4:
+            schet = 3
+        else:
+            schet = schet - 1
+        for i in range(schet):
+            q = i + 1
+            find = h_data[-q]
+            find = find.split('@#$')
+            firstr = ''.join([str(q), ') Пример: ', str(find[0])])
+            secstr = ''.join(['Ответ: ', str(find[1])])
+            text.append('\n'.join([firstr, secstr]))
+        text = '\n \n'.join(text)
+        self.label_5.setText(text)
+        h_data = '\n'.join(h_data)
+        f.write(h_data)
+        f.close()
+        return
 
 
 app = QApplication(sys.argv)
